a50b18804da327209a24a0a6733b731841df4d34
Toru <toru959@gmail.com>
2025-07-21 09:34:15 +0900

    instance profile

--- iam.tf
+++ iam.tf
@@ -1,4 +1,9 @@
 #IAM Role
+resource "aws_iam_instance_profile" "app_ec2_profile" {
+  name = aws_iam_role.app_iam_role.name
+  role = aws_iam_role.app_iam_role.name
+}
+
 resource "aws_iam_role" "app_iam_role" {
   name               = "${var.project}-${var.environment}-app-iam-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
@@ -34,3 +39,5 @@ resource "aws_iam_role_policy_attachment" "app_iam_role_s3_readonly" {
   role       = aws_iam_role.app_iam_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
+
+
