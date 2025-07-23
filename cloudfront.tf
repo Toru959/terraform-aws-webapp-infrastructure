14ed67cf33e26034e5bfc885c973ddac567dcadb
Toru <toru959@gmail.com>
2025-07-23 11:08:18 +0900

    cloudfront02

--- cloudfront.tf
+++ cloudfront.tf
@@ -11,9 +11,19 @@ resource "aws_cloudfront_distribution" "cf" {
 
     custom_origin_config {
       origin_protocol_policy = "match-viewer"
-      origin_ssl_protocols    = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-      http_port               = 80
-      https_port              = 443
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      http_port              = 80
+      https_port             = 443
+    }
+  }
+
+  origin {
+    domain_name = aws_s3_bucket.s3_static_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.s3_static_bucket.id
+
+    s3_origin_config {
+#      origin_access_identity = aws_cloudfront_origin_access_identity.cf_s3_origin_access_identity.cloudfront_access_identity_path
+	origin_access_identity = ""
     }
   }
 
@@ -28,11 +38,32 @@ resource "aws_cloudfront_distribution" "cf" {
       }
     }
 
-    target_origin_id      = aws_lb.alb.name
+    target_origin_id       = aws_lb.alb.name
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 0
     max_ttl                = 0
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/pablic/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.s3_static_bucket.id
+
+    forwarded_values {
+      query_string = false
+      headers      = []
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 3156000
+    compress               = true
   }
 
   restrictions {
@@ -41,13 +72,17 @@ resource "aws_cloudfront_distribution" "cf" {
     }
   }
 
-  aliases = ["dev.${var.domain}"]
+#  aliases = ["dev.${var.domain}"]
 
   viewer_certificate {
-#    acm_certificate_arn   = aws_acm_certificate.virginia_cert.arn
-#    minimum_protocol_version = "TLSv1.2_2019"
-#    ssl_support_method       = "sni-only"
+    #    acm_certificate_arn   = aws_acm_certificate.virginia_cert.arn
+    #    minimum_protocol_version = "TLSv1.2_2019"
+    #    ssl_support_method       = "sni-only"
   }
+}
+
+resource "aws_cloudfront_origin_access_identity" "cf_s3_origin_access_identity" {
+  comment = "S3 static bucket access identity"
 }
 
 resource "aws_route53_record" "route53_cloudfront" {
