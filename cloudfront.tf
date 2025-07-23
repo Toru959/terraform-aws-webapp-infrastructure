1972c8d6b6821643a1529d3c1417531dfb162261
Toru <toru959@gmail.com>
2025-07-23 10:27:26 +0900

    modify

--- cloudfront.tf
+++ cloudfront.tf
@@ -44,9 +44,9 @@ resource "aws_cloudfront_distribution" "cf" {
   aliases = ["dev.${var.domain}"]
 
   viewer_certificate {
-    acm_certificate_arn   = aws_acm_certificate.virginia_cert.arn
-    minimum_protocol_version = "TLSv1.2_2019"
-    ssl_support_method       = "sni-only"
+#    acm_certificate_arn   = aws_acm_certificate.virginia_cert.arn
+#    minimum_protocol_version = "TLSv1.2_2019"
+#    ssl_support_method       = "sni-only"
   }
 }
 
